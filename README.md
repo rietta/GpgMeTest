c85e1a440118bf1b3ff07b09c2e0c56ba6907003
Frank Rietta <frank@rietta.com>
2014-05-04 00:43:42 -0400

    Post has some virtual attributes.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Experimental Record with GPGME
+
+PostgreSQL has GPG support by default too.
+
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,28 @@
-class EncryptedPost < EncryptedMessage
+class Post < EncryptedMessage
+  
   attr_accessor :subject, :body, :url
   
+  validates :subject, presence: true
+  validates :body, presence: true
+  
+  def structured_plaintext
+    {
+      subject: subject,
+      body: body,
+      url: url,
+    }
+  end
+  
+  def body_plaintext
+"
+Subject:
+#{subject}
+
+URL:
+#{url}
+
+Message:
+#{body}
+"
+  end
 end
