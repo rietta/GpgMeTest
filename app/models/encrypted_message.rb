1f08ad30b8cf1bb04eb0ae09ba28338bbd1eb265
Frank Rietta <frank@rietta.com>
2014-05-04 22:21:19 -0400

    The encrypted messages to the intended recipients and the backup key.

--- app/models/encrypted_message.rb
+++ app/models/encrypted_message.rb
@@ -14,30 +14,39 @@
 
 class EncryptedMessage < ActiveRecord::Base
   MUST_IMPLEMENT = 'Must be implemented by a subclass.'
-  
+
   # The database does not let these fields be nil, so the presence is needed.
   validates :delete_at, presence: true
   validates :type, presence: true
-  
+
+  ##
+  # Encrypt the text to the authorized recipients using GnuPG.
+  # The keys need to be present in the local GPG keyring.
   def encrypt_text(text)
     crypto = GPGME::Crypto.new(armor: true)
-    crypto.encrypt(text, recipients: recipient_keys)
+    crypto.encrypt(text, recipients: recipient_keys).to_s
   end
-  
+
   #############################
   protected
-  
+
+  ##
   # The keys of the parties to receive this data
+  #
   # This could presumably be specified in a database, rather than hard coded.
-  # The choice is left to the developer who is adopting this test to their own purposes.
+  # The choice is left to the developer who is adopting this test to their own purposes. In business environment, typically three or more keys are used. The recipients who are authorized and a backup key for the company, that is kept under seal.
   def recipient_keys
-    ['frank@rietta.com']
+    [
+      '0x0CD8B255', # Frank Rietta
+      '0x91C8FEBF', # Brandon Dees
+      '0xFD019213', # Security Team, Serving as the Backup Key
+    ]
   end
-  
+
   def structured_plaintext
     raise MUST_IMPLEMENT
   end
-  
+
   def body_plaintext
     raise MUST_IMPLEMENT
   end
