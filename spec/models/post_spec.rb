7754370f244c7de042d70cc572d9b6891342c956
Frank Rietta <frank@rietta.com>
2014-05-05 00:09:55 -0400

    Test for the expiration date settings.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,14 +1,23 @@
 require 'spec_helper'
 
 describe 'Encrypted Post' do
+
+  before(:each) do
+    @basics = {
+      plaintext_description: 'Hello message.',
+      body: 'Hello, world.',
+      subject: 'This is a test',
+      url: 'http://rietta.com/blog/'
+    }
+  end
+
   it 'Can encrypt a message' do
-    post = Post.new(
-        plaintext_description: 'Hello message.',
-        delete_at: Date.today + 2.years,
-        body: 'Hello, world.',
-        subject: 'This is a test',
-        url: 'http://rietta.com/blog/'
-      )
+    post = Post.new(@basics.merge(delete_at: Date.today + 2.years))
     post.should be_valid
+  end
+
+  it 'Cannot save a message that expires in the past' do
+    post = Post.new(@basics.merge(delete_at: Date.today - 1.day))
+    post.should be_invalid
   end
 end
