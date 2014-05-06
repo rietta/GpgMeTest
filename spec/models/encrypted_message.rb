00acda8cdecc514430f807b00907fdb34aeee640
Frank Rietta <frank@rietta.com>
2014-05-05 22:26:02 -0400

    Data retention policy tests.

--- spec/models/encrypted_message.rb
+++ spec/models/encrypted_message.rb
@@ -18,4 +18,45 @@ describe EncryptedMessage do
     end
 
   end # testing PGP encrypted
+
+
+  context 'Expiring Messages' do
+    before(:each) do
+      @base_attr = {
+              plaintext_description: "Hello message.",
+              type: 'Post',
+              delete_at: Date.today,
+              encrypted_body: "-----BEGIN PGP MESSAGE-----\nVersion: GnuPG v1\n\nhQIMA4Ybj5/9AZITAQ//XbsQE6NYE87q0uYw4GLXveOCLicp3E3lh+GgQujDy0ao\nMvcSvAzOED+Ii+U/NqhXo3sjYGDqd/U00bh/IYRlm7aAkFnAIgrDFI2Ci/RLM\ncaYGKCEFl4HAupATWdyQa/1P3R+3O67hL2SC\n=BEK/\n-----END PGP MESSAGE-----\n",
+       encrypted_structured_body: "-----BEGIN PGP MESSAGE-----\nVersion: GnuPG v1\n\nhQIMA4Ybj5/9AZITARAAjUxVHFNBTkSMiVx5Pa9bNJXyxYMJ+0DHIepOk5h3/6GA\nlBlGvNJsYKyOmVw0Xbag8vDMSBkrAFuaynBn6BqAWZ8KBMA1Q5cKpFzX8YtL0\n03LsMbbxAxc31XqMdAYd8GfEPZIwdCLrOCuJVA==\n=11c2\n-----END PGP MESSAGE-----\n"
+         }
+
+    end # each
+
+
+    it 'Deleting cases expired before today' do
+      messages = [
+        EncryptedMessage.new(@base_attr.merge(delete_at: Date.today - 2.days)),
+        EncryptedMessage.new(@base_attr.merge(delete_at: Date.today - 1.days)),
+        EncryptedMessage.new(@base_attr.merge(delete_at: Date.today)),
+        EncryptedMessage.new(@base_attr.merge(delete_at: Date.today + 1.days)),
+        EncryptedMessage.new(@base_attr.merge(delete_at: Date.today + 2.days)),
+      ]
+
+      EncryptedMessage.transaction do
+        messages.each do |m|
+          m.save(validate: false)
+        end
+
+        EncryptedMessage.count.should == 5
+        EncryptedMessage.expired.count.should == 2
+        EncryptedMessage.delete_expired
+        EncryptedMessage.count.should == 3
+        EncryptedMessage.expired.count.should == 0
+
+        EncryptedMessage.delete_all
+      end
+    end # delete
+
+  end #
+
 end
